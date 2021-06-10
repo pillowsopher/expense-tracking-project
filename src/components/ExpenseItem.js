31ad617e915566ee2bdf57cf5139ba23f268d6ba
pillowsopher <sarthakrege@gmail.com>
2021-06-10 16:19:40 -0700

    Passed in expenses using Props
    Passed in data in expenses using props instead of hard-coded data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  return <h1>Added an expense Item!</h1>;
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item-card'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount} </div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
